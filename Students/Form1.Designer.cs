5ee963511018b14a5aec91b952943df024e16af2
vataaly <zsolt4205@gmail.com>
2026-02-09 12:50:47 +0100

    Add project files.

---
+++ Students/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Students
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadCsv = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadCsv
+            // 
+            this.btnLoadCsv.Location = new System.Drawing.Point(32, 183);
+            this.btnLoadCsv.Name = "btnLoadCsv";
+            this.btnLoadCsv.Size = new System.Drawing.Size(133, 23);
+            this.btnLoadCsv.TabIndex = 0;
+            this.btnLoadCsv.Text = "CSV betöltése";
+            this.btnLoadCsv.UseVisualStyleBackColor = true;
+            this.btnLoadCsv.Click += new System.EventHandler(this.btnLoadCsv_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(232, 84);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 1;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(232, 171);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(232, 264);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 20);
+            this.txtAge.TabIndex = 3;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(232, 370);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(109, 23);
+            this.btnSubmit.TabIndex = 4;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(454, 134);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(283, 190);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(264, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(267, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(267, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Age";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnLoadCsv);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadCsv;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Students/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CsvHelper;
+using System.Globalization;
+using System.IO;
+using MySql.Data.MySqlClient;
+
+
+namespace Students
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+            EnsureDatabaseAndTable();
+            LoadStudentsToGrid();
+        }
+        private void EnsureDatabaseAndTable()
+        {
+            
+            using (var conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+
+                string sql = @"
+                CREATE TABLE IF NOT EXISTS Students (
+                    Id INT AUTO_INCREMENT PRIMARY KEY,
+                    Name VARCHAR(100) NOT NULL,
+                    Email VARCHAR(100) NOT NULL,
+                    Age INT NOT NULL
+                );";
+
+                using (var cmd = new MySqlCommand(sql, conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private string connStr = "server=127.0.0.1;port=3307;database=studentdb;user=root;password=;";
+
+        private List<Student> ReadCsv(string path)
+        {
+            using (var reader = new StreamReader(path))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                return csv.GetRecords<Student>().ToList();
+            }
+        }
+
+        private void SaveStudentsToDatabase(List<Student> students)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+
+                foreach (var s in students)
+                {
+                    string query = "INSERT INTO Students (Name, Email, Age) VALUES (@n, @e, @a)";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@n", s.Name);
+                    cmd.Parameters.AddWithValue("@e", s.Email);
+                    cmd.Parameters.AddWithValue("@a", s.Age);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        private void LoadStudentsToGrid()
+        {
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                conn.Open();
+
+                MySqlDataAdapter da = new MySqlDataAdapter(
+                    "SELECT Id, Name, Email, Age FROM Students", conn);
+
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                dataGridView1.DataSource = dt;
+                dataGridView1.AutoGenerateColumns = true;
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+                dataGridView1.ReadOnly = true;
+                dataGridView1.AllowUserToAddRows = false;
+                dataGridView1.Refresh();
+            }
+        }
+
+        private void txtName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLoadCsv_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "CSV files (*.csv)|*.csv";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var students = ReadCsv(ofd.FileName);
+                    SaveStudentsToDatabase(students);
+                    LoadStudentsToGrid();
+                    MessageBox.Show("CSV sikeresen betöltve!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Hiba történt: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtName.Text) ||
+    string.IsNullOrWhiteSpace(txtEmail.Text))
+            {
+                MessageBox.Show("Név és email kötelező!");
+                return;
+            }
+
+            if (!int.TryParse(txtAge.Text, out int age))
+            {
+                MessageBox.Show("Az életkor csak szám lehet!");
+                return;
+            }
+
+            try
+            {
+                using (var conn = new MySql.Data.MySqlClient.MySqlConnection(connStr))
+                {
+                    conn.Open();
+                    string q = "INSERT INTO Students (Name, Email, Age) VALUES (@n, @e, @a)";
+                    var cmd = new MySql.Data.MySqlClient.MySqlCommand(q, conn);
+                    cmd.Parameters.AddWithValue("@n", txtName.Text);
+                    cmd.Parameters.AddWithValue("@e", txtEmail.Text);
+                    cmd.Parameters.AddWithValue("@a", age);
+                    cmd.ExecuteNonQuery();
+                }
+
+                LoadStudentsToGrid();
+                txtName.Clear();
+                txtEmail.Clear();
+                txtAge.Clear();
+                MessageBox.Show("Sikeres mentés!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hiba mentéskor: " + ex.Message);
+            }
+        }
+    }
+}
